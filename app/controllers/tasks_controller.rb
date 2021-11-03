7d67b93d572e12a7e464d89506919b6c2cafe29c
Tatsunori <tatsunori.palmeaves@gmail.com>
2021-11-03 19:25:58 +0900

    ✅  　編集、更新、削除機能の追加
    edit,update,destroyメソッドの追加とビューファイルの更新

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,13 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
   end
 
   def create
@@ -20,7 +27,14 @@ class TasksController < ApplicationController
     redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
+  end
+
 private
+
   def task_params
     params.require(:task).permit(:name, :description)
   end
